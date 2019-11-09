3827f331f94ab76e172447ae7e5cf94b14ad9be6
brennobemoura <brennobemoura@outlook.com>
2019-11-08 22:48:41 -0300

    Added Scroll

---
+++ UIContainer/Classes/Helpers/Scroll.swift
@@ -0,0 +1,48 @@
+//
+//  Scroll.swift
+//  UIContainer
+//
+//  Created by brennobemoura on 08/11/19.
+//
+
+import Foundation
+import UIKit
+
+public class Scroll: UIScrollView {
+    public enum Axis {
+        case vertical
+        case horizontal
+        case auto(vertical: UILayoutPriority, horizontal: UILayoutPriority)
+    }
+
+    public init(_ view: UIView, axis: Axis = .vertical) {
+        super.init(frame: .zero)
+        self.addSubview(view)
+        view.snp.makeConstraints {
+            $0.edges.equalTo(0)
+        }
+
+        self.setAxis(axis)
+    }
+
+    public func setAxis(_ axis: Axis) {
+        self.subviews.first!.snp.remakeConstraints {
+            $0.edges.equalTo(0)
+            switch axis {
+            case .vertical:
+                $0.width.equalTo(self.snp.width).priority(.required)
+                $0.height.equalTo(self.snp.height).priority(.low)
+            case .horizontal:
+                $0.height.equalTo(self.snp.width).priority(.required)
+                $0.width.equalTo(self.snp.height).priority(.low)
+            case .auto(let vertical, let horizontal):
+                $0.height.equalTo(self.snp.width).priority(vertical)
+                $0.width.equalTo(self.snp.height).priority(horizontal)
+            }
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
