a0558fa9b5c261014c8cca8defd8f85ad6f1227b
brennobemoura <brennobemoura@outlook.com>
2019-12-23 18:03:30 -0300

    UICreator fixes

--- UIContainer/Classes/Container/ContainerView.swift
+++ UIContainer/Classes/Container/ContainerView.swift
@@ -30,6 +30,10 @@ open class ContainerView<View: UIView & ContainerViewParent>: ContainerBox, Cont
         self.prepareContainer(inside: parentView, loadHandler: loadHandler)
         self.containerDidLoad()
     }
+
+    public required init() {
+        super.init(frame: .zero)
+    }
     
     required public init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
